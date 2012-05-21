a8a0e3c79319bb136d153d793fcfb7326e5bb029
William Hu (Macbook pro) <hu.will38@gmail.com>
2012-05-21 16:04:06 +0800

    Minor details Changed minor details in UI added a few files to git

---
+++ src/com/hook38/sporttimer/CountdownTimerHelpActivity.java
@@ -0,0 +1,14 @@
+package com.hook38.sporttimer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CountdownTimerHelpActivity extends Activity {
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.countdowntimer_help);
+        
+	}
+}
---
+++ src/com/hook38/sporttimer/StopwatchHelpActivity.java
@@ -0,0 +1,13 @@
+package com.hook38.sporttimer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StopwatchHelpActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.stopwatch_help);
+	}
+}
